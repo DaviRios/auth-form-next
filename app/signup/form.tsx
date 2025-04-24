c058d21857c4919afd710fae08be663226787a2a
Davi Rios dos Santos <AstroSurfer.50@Gmail.com>
2025-04-24 17:10:56 -0300

    Create project (auth form with next) + remove default files + zod add for future auth method(Learning zod too)

---
+++ app/signup/form.tsx
@@ -0,0 +1,13 @@
+import { signup } from '@/app/signup/actions'
+
+export function SignUpForm(){
+    return (
+        <form action={signup}>
+            <input name='name'></input>
+            <input name='email'></input>
+            <input name='password'></input>
+
+            <button>SignUp</button>
+        </form>
+    )
+}
