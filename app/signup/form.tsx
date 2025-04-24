b5814a3de807cfca008d2bb31ea7b39f2228ca2d
Davi Rios dos Santos <AstroSurfer.50@Gmail.com>
2025-04-24 17:46:32 -0300

    change min name for user and add state error verify

--- app/signup/form.tsx
+++ app/signup/form.tsx
@@ -1,11 +1,19 @@
+'use client'
+
 import { signup } from '@/app/signup/actions'
 
 export function SignUpForm() {
+    const [state, action] = userActionState(signup)
+
     return (
         <form action={signup}>
             <input name='name'></input>
+            {/* verify erros in name,email and password and add to state */}
+            {state?.erros?.name && <p>{state.errors.name}</p>} 
             <input name='email'></input>
+            {state?.erros?.email && <p>{state.errors.email}</p>}
             <input name='password'></input>
+            {state?.erros?.password && <p>{state.errors.password}</p>}
 
             <button>SignUp</button>
         </form>
