c71fca6570c9323ad54f7478e0e424dac3293b04
Davi Rios dos Santos <AstroSurfer.50@Gmail.com>
2025-06-16 12:14:08 -0300

    install and add drizzle for user admin role

---
+++ drizzle-next.config.ts
@@ -0,0 +1,14 @@
+const drizzleNextConfig = {
+  "version": "0.0.18",
+  "packageManager": "npm",
+  "latest": false,
+  "dbDialect": "postgresql",
+  "dbPackage": "pg",
+  "pkStrategy": "uuidv4",
+  "authEnabled": false,
+  "adminEnabled": false,
+  "install": true,
+  "pluralizeEnabled": true
+};
+
+export default drizzleNextConfig;
